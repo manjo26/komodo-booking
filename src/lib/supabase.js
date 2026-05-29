47e9895df9ff7415ddbec0c97c3dee5e029aa9b3
kakajo <kakajo250@gmail.com>
2026-05-29 22:49:56 +0800

    fix supabase global

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl =
-process.env.NEXT_PUBLIC_SUPABASE_URL
+process.env.NEXT_PUBLIC_SUPABASE_URL || ""
 
 const supabaseKey =
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 
 export const supabase =
-createClient(supabaseUrl, supabaseKey)
+supabaseUrl && supabaseKey
+? createClient(supabaseUrl, supabaseKey)
+: null
